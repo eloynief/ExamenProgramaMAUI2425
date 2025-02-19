028008eb8ab743d8c09a32dc7eb8e0ec8cc3f3fd
Eloy Nieves Fernández <eloy.nieves@iesnervion.es>
2025-02-19 14:22:09 +0100

    Agregar archivos de proyecto.

---
+++ UIasp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UIasp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ UImaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace UImaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
